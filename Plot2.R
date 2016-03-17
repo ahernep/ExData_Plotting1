f5cb85886c9f8ea48511a178820f58ba5087777e
Peter Aherne <ahernep@gmail.com>
2016-03-17 10:15:41 +0800

    New upload

---
+++ Plot2.R
@@ -0,0 +1,10 @@
+genPlot2 <- function() {
+  
+  powerData <- read.csv("household_power_consumption.txt", sep=";", header=TRUE)
+  powerData$Date <- as.Date(as.character(powerData$Date), "%d/%m/%Y")
+  powerData <- subset(powerData, powerData$Date >= as.Date("2007-02-01") & 
+                        powerData$Date <= as.Date("2007-02-02"))
+  ## png(filename="Plot1.png", width = 480, height = 480)
+  ##plot time
+  
+}
